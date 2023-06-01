7e9bd8a9b7df43f6cbc4f2e9f547d77ec6a8c808
Serhan Erdemir <120390750+erdemirserhan@users.noreply.github.com>
2023-06-01 09:19:54 +0200

    Update jboss.sh

--- jboss.sh
+++ jboss.sh
@@ -9,3 +9,8 @@ cd /opt/jboss/standalone/configuration
 
 # Debug standalone.xml and all the other config files
 cd /opt/jboss/wildfly/standalone/configuration
+
+# Wait until you make a connection to the jboss servers via 9993 https
+(/opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 &) &&\
+        while true; do sleep 2 ; curl -sk http://127.0.0.1:9993 > /dev/null ; \
+       [ $? -eq 0 ] && break; done &&\
