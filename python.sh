f0f88431da110d985bf265d6694af7d6bde9869c
Serhan Erdemir <120390750+erdemirserhan@users.noreply.github.com>
2023-06-19 09:23:34 +0200

    Create python.sh

---
+++ python.sh
@@ -0,0 +1,11 @@
+# Open a git-bash terminal in windows and execute following to activate pre-commit
+
+mkdir precommittest
+cd precommittest
+<path to installed python exe> -m venv venv
+. ./venv/Scripts/activate
+python -m pip install pip --upgrade
+python -m pip install pre-commit
+pre-commit sample-config > .pre-commit-config.yaml
+pre-commit install
+pre-commit run --all-files
