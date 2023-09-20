3ffd202851b8e05c3ccf06599f19a68df1247f08
Serhan Erdemir <120390750+erdemirserhan@users.noreply.github.com>
2023-09-20 09:10:23 +0200

    Update sed.sh

--- sed.sh
+++ sed.sh
@@ -12,3 +12,8 @@ sed -i -e "s/^#\?<yourstringhere>.*/<yourstringhere> = <newvaluehere>/g
 # ^#\?<yourstringhere>.* matches any line that starts with an optional "#" character followed by "<yourstringhere>" and any other characters.
 # yourstringhere> = <newvaluehere> is the replacement string, which replaces the matched text in the input file.
 # /g indicates that the replacement should be performed globally, i.e., on all occurrences of the matched pattern in the file.
+
+
+# Use the sed command to add a line at the end of a file by using the a (append) command.
+sed -i '$a\This is the line I want to add' your_file.txt
+
