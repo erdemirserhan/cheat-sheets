bbb65bb0b8359ce1981a141874b706b8dff99986
Serhan Erdemir <120390750+erdemirserhan@users.noreply.github.com>
2023-10-16 11:27:22 +0200

    Update helm.sh

--- helm.sh
+++ helm.sh
@@ -1,2 +1,8 @@
 # pull a chart from remote helm-registry
 helm pull oci://<your.repo.url.here>/path/to/your/chart --version <helm_chart_version_here>
+
+# lint a chart
+helm lint .
+
+# make a new chart with the given chart, also with line numbers to debug
+helm template . --debug | grep -n '^' 
