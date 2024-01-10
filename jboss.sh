d1fa0e3fdd818b820f8601609c3e5c698cc4f27d
Serhan Erdemir <120390750+erdemirserhan@users.noreply.github.com>
2024-01-10 09:58:41 +0100

    Update jboss.sh

--- jboss.sh
+++ jboss.sh
@@ -4,6 +4,9 @@
 # Restart/reload server config on jboss wildfly via cli
 /opt/jboss/wildfly/bin/jboss-cli.sh --connect -c ":shutdown(restart=true)"
 
+# Restart/reload server config on jboss via cli with tls (9993 is tls for management url)
+/opt/jboss/bin/jboss-cli.sh --connect --controller=remote+https://localhost:9993 -c ":shutdown(restart=true)"
+
 # Debug standalone.xml and all the other config files
 cd /opt/jboss/standalone/configuration
 
