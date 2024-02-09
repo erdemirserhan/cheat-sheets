0eaa32fb235bd7984be90294b712b54e950b2dec
Serhan Erdemir <120390750+erdemirserhan@users.noreply.github.com>
2024-02-09 16:00:54 +0100

    Update kafka.sh

--- kafka.sh
+++ kafka.sh
@@ -9,3 +9,15 @@
     --bootstrap-server <bootstrap_server>:9092 \
     --delete \
     --topic 'your_topic_name-.*'
+
+# List all topics with SSL
+./kafka-topics.sh --bootstrap-server <bootstrap_server>:9093 --list --command-config /path/to/your/client.properties
+
+# Basic client.properties
+security.protocol=SSL
+ssl.keymanager.algorithm=SunX509
+ssl.keystore.type=PKCS12 
+ssl.keystore.location=/path/to/your/client/certificate.p12 # if in Kubernetes, first decode from base64 within echo "<text" | base64 --decode
+ssl.keystore.password=<client_password>  # if in Kubernetes, first decode from base64 within echo "<text" | base64 --decode
+ssl.truststore.location=/path/to/your/server/certificate/ca.crt
+ssl.truststore.type=PEM
