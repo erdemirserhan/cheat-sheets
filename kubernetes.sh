0db495550bb3983a52193237d78bc2bb753cae73
Serhan Erdemir <120390750+erdemirserhan@users.noreply.github.com>
2024-03-14 09:13:25 +0100

    Update kubernetes.sh

--- kubernetes.sh
+++ kubernetes.sh
@@ -4,3 +4,5 @@ kubectl cp <namespace>/<pod_full_name>:/path/to/your/files/myfile.xml myfile.xml
 
 # Copy a file from your local Machine to a Kubernetes-Pod
 kubectl cp myfile.xml <namespace>/<pod_full_name>:/path/to/your/files/myfile.xml 
+# Delete every deployment with a name containt <word>
+kubectl get deployments -n=<your_namespace> | grep '<word>' | awk '{print $1}' | xargs kubectl delete deployment -n=<your_namespace>
