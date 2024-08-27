d8de3c7e1c4c346afa635da03569a5bdf7a614bd
serhan.erdemir@adesso-insurance-solutions.de <serhan.erdemir@adesso-insurance-solutions.de>
2024-08-27 17:33:17 +0200

    main add

--- helm.sh
+++ helm.sh
@@ -5,4 +5,7 @@ helm pull oci://<your.repo.url.here>/path/to/your/chart --version <helm_chart_ve
 helm lint .
 
 # make a new chart with the given chart, also with line numbers to debug
-helm template . --debug | grep -n '^' 
+helm template . --debug | grep -n '^'
+
+# get the dependencies that is defined in the chart.yaml
+helm dependency build
